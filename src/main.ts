d403a0848118bfb76008a79cfcc370c542c184b4
German <germanpidote@gmail.com>
2022-08-07 16:54:05 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { enviroment } from "./config/enviroment";
 async function main() {
   dotenv.config();
 
-  connect();
+ // connect();
   const port: number = Number(enviroment.PORT) || 8080 ; 
   const app = await startServer();
   app.listen(port); //si quisiera hacerlo en la lan despues del port, coloco el ipv4
