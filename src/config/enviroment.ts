583ef3bd4ce211063e46292b371b4a93dac67e91
German <germanpidote@gmail.com>
2022-08-09 11:29:54 -0300

    update

--- src/config/enviroment.ts
+++ src/config/enviroment.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 export const enviroment = {
   PORT: process.env.PORT,
-  DB_HOST: process.env.HOST,
+  DB_HOST: process.env.DB_HOST,
   DB_PORT: process.env.DB_PORT,
   DB_USERNAME: process.env.DB_USERNAME,
   DB_PASSWORD: process.env.DB_PASSWORD,
