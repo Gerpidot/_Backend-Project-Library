bad7205fa67ac018581cbd56b029d724e8b88394
German <germanpidote@gmail.com>
2022-08-09 12:01:30 -0300

    update

--- src/config/enviroment.ts
+++ src/config/enviroment.ts
@@ -2,8 +2,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 export const enviroment = {
+  DATABASE_URL:"postgres",
   PORT: process.env.PORT,
-  DB_HOST: "ec2-50-19-255-190.compute-1.amazonaws.com",
+  DB_HOST: process.env.DB_HOST,
   DB_PORT: process.env.DB_PORT,
   DB_USERNAME: process.env.DB_USERNAME,
   DB_PASSWORD: process.env.DB_PASSWORD,
--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -5,12 +5,15 @@ import { enviroment } from "./enviroment";
 export async function connect() {
   await createConnection({
     type: "postgres",
-    port: Number(enviroment.DB_PORT),
-    username: enviroment.DB_USERNAME,
-    password: enviroment.DB_PASSWORD,
-    database: enviroment.DB_DATABASE,
+    url: enviroment.DATABASE_URL,
+    //port: Number(enviroment.DB_PORT),
+    //username: enviroment.DB_USERNAME,
+    //password: enviroment.DB_PASSWORD,
+    //database: enviroment.DB_DATABASE,
+    extra:{ssl:true},
     entities: [path.join(__dirname, "../entity/**/**.ts")],
     synchronize: true,
+    
     
   });
   
