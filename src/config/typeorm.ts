3f7014bbe9c0d5b2587bee3e2f3851875bcc526e
German <germanpidote@gmail.com>
2022-08-09 12:11:41 -0300

    update

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -6,12 +6,12 @@ export async function connect() {
   await createConnection({
     type: "postgres",
     url: enviroment.DATABASE_URL,
-    //port: Number(enviroment.DB_PORT),
-    //username: enviroment.DB_USERNAME,
-    //password: enviroment.DB_PASSWORD,
-    //database: enviroment.DB_DATABASE,
+    /*port: Number(enviroment.DB_PORT),
+    username: enviroment.DB_USERNAME,
+    password: enviroment.DB_PASSWORD,
+    database: enviroment.DB_DATABASE,*/
     extra:{ssl:true},
-    //entities: [path.join(__dirname, "../entity/**/**.ts")],
+    entities: [path.join(__dirname, "../entity/**/**.ts")],
     synchronize: true,
     
     
