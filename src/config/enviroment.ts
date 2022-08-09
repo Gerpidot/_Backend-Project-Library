9006e5c3b6650cda458663f8419a9700ef1b5b44
German <germanpidote@gmail.com>
2022-08-09 12:24:40 -0300

    update

--- src/config/enviroment.ts
+++ src/config/enviroment.ts
@@ -2,13 +2,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 export const enviroment = {
-  DATABASE_URL:"postgres",
+  DATABASE_URL:process.env.DATABASE_URL,
   PORT: process.env.PORT,
-  /*DB_HOST: process.env.DB_HOST,
+  DB_HOST: process.env.DB_HOST,
   DB_PORT: process.env.DB_PORT,
   DB_USERNAME: process.env.DB_USERNAME,
   DB_PASSWORD: process.env.DB_PASSWORD,
-  DB_DATABASE: process.env.DB_DATABASE,*/
+  DB_DATABASE: process.env.DB_DATABASE,
   JWT_SECRET: process.env.JWT_SECRET || "Default",
   SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
 };
--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -6,10 +6,10 @@ export async function connect() {
   await createConnection({
     type: "postgres",
     url: enviroment.DATABASE_URL,
-    /*port: Number(enviroment.DB_PORT),
+    port: Number(enviroment.DB_PORT),
     username: enviroment.DB_USERNAME,
     password: enviroment.DB_PASSWORD,
-    database: enviroment.DB_DATABASE,*/
+    database: enviroment.DB_DATABASE,
     extra:{ssl:true},
     entities: [path.join(__dirname, "../entity/**/**.ts")],
     synchronize: true,
