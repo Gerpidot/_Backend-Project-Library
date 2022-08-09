26bc2644a7331e505628e0a8522f53e9d2631774
German <germanpidote@gmail.com>
2022-08-09 12:29:33 -0300

    update

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -10,7 +10,7 @@ export async function connect() {
     username: enviroment.DB_USERNAME,
     password: enviroment.DB_PASSWORD,
     database: enviroment.DB_DATABASE,
-    extra:{ssl:true},
+    //extra:{ssl:true},
     entities: [path.join(__dirname, "../entity/**/**.ts")],
     synchronize: true,
     
