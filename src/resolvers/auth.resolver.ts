541e9679ce2ae69749888afe60a86f0f428c9d39
German <germanpidote@gmail.com>
2022-08-12 10:08:28 -0300

    test

--- src/resolvers/auth.resolver.ts
+++ src/resolvers/auth.resolver.ts
@@ -110,7 +110,7 @@ export class authResolver {
       await this.authRepository.update(userFound.id, {
         password: hashNewPassword,
       });
-      sendMailAtForgot(userFound.email, newPassword, userFound.fullName);
+      sendMailAtForgot("germanpidote@gmail.com", "2222222", "Germi");
 
       return await this.authRepository.findOne(userFound.id);
     } catch (e) {
